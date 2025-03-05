0a522178b5a551ba36ceb60070b8aac645ccca09
James Leatherman <james.leatherman@gmail.com>
2025-03-05 18:47:22 +0000

    fix quotes

--- terraform-code/modules/info-page/variables.tf
+++ terraform-code/modules/info-page/variables.tf
@@ -1,5 +1,5 @@
 variable "run_provisioners" {
-  description = "Choice to run provisioners""
-  type    = bool
-  default = false
+  description = "Choice to run provisioners"
+  type        = bool
+  default     = false
 }

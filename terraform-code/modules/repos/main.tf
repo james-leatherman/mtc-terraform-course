250480fa7af53b23deb2dc2807f2f7f9ef8c69ab
James Leatherman <james.leatherman@gmail.com>
2025-03-05 13:00:36 +0000

    refactor for automation

---
+++ terraform-code/modules/repos/main.tf
@@ -0,0 +1,38 @@
+resource "local_file" "repos" {
+  content  = jsonencode(local.repos)
+  filename = "${path.module}/repos.json"
+}
+
+module "repos" {
+  source    = "./modules/dev-repos"
+  for_each  = var.environments
+  repos_max = 10
+  env       = each.key
+  # repos            = jsondecode(file("repos.json"))
+  repos            = { for v in csvdecode(file("repos.csv")) : v["environment"] => { for x, y in v : x => lower(y) } }
+  run_provisioners = false
+}
+
+module "deploy-keys" {
+  for_each  = var.deploy_key ? toset(flatten([for k, v in module.repos : keys(v.clone-urls) if k == "dev"])) : []
+  source    = "./modules/deploy-keys"
+  repo_name = each.key
+}
+
+# module "info-page" {
+#   source           = "./modules/info-page"
+#   repos            = { for k, v in module.repos["prod"].clone-urls : k => v }
+#   run_provisioners = false
+# }
+
+output "repo-info" {
+  value = { for k, v in module.repos : k => v.clone-urls }
+}
+
+output "repo-list" {
+  value = flatten([for k, v in module.repos : keys(v.clone-urls) if k == "dev"])
+}
+
+output "clone_urls" {
+  value = module.repos
+}
